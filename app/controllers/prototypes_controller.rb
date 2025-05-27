b035466b5f5441e448dc723074649a39a02343ed
naecchi <71973149+naecchi@users.noreply.github.com>
2025-05-27 21:16:48 +0900

    アクセス範囲の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,10 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]  
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
   
   def update
